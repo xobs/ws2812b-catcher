057ddae79f86b6a6a436300b60fa3da44b501ec8
Sean Cross <sean@xobs.io>
2018-08-02 15:50:14 +0800

    ws2812b-catcher: commit non-working WIP version
    Signed-off-by: Sean Cross <sean@xobs.io>

--- ws2812b-catcher.py
+++ ws2812b-catcher.py
@@ -18,21 +18,34 @@ class ClockGen(Module):
         self.clock_domains.cd_sys150 = ClockDomain(reset_less=True)
 
         clk12 = platform.request("clk12")
-        rst = Signal()
-        #self.clk50 = Signal()
+        self.cd_sys.clk = clk12
+        sys150 = Signal()
+
+        pll_locked_unbuffered = Signal()
+        pll_locked_led = platform.request("user_led", 1)
+        pll_locked = Signal()
+
+        self.comb += [
+            pll_locked_led.eq(pll_locked)
+        ]
 
         self.specials += [
             Instance("SB_PLL40_CORE",        
                 p_FEEDBACK_PATH="SIMPLE",
                 p_PLLOUT_SELECT="GENCLK",
                 p_DIVR=0,
-                p_DIVF=49,
-                p_DIVQ=2,
+                p_DIVF=79,
+                p_DIVQ=4,
                 p_FILTER_RANGE=1,
 
                 i_BYPASS=0, i_RESETB=1,
-                i_REFERENCECLK=clk12, o_PLLOUTCORE=self.cd_sys150.clk,
+                i_REFERENCECLK=clk12,
+
+                o_PLLOUTGLOBAL=sys150,
+                o_LOCK=pll_locked_unbuffered
             ),
+            Instance("SB_GB", i_USER_SIGNAL_TO_GLOBAL_BUFFER=sys150, o_GLOBAL_BUFFER_OUTPUT=self.cd_sys150.clk),
+            Instance("SB_GB", i_USER_SIGNAL_TO_GLOBAL_BUFFER=pll_locked_unbuffered, o_GLOBAL_BUFFER_OUTPUT=pll_locked),
         ]
 
 class IceStickSpecCatcherPlatform(Platform):
@@ -40,64 +53,73 @@ class IceStickSpecCatcherPlatform(Platform):
         Platform.__init__(self)
 
         self.add_extension([
-            ("rgbled", 0, Pins("44"), IOStandard("LVCMOS33"))
+            ("rgbled", 0, Pins("44"), IOStandard("LVCMOS33")),
+            ("rawclock", 0, Pins("45"), IOStandard("LVCMOS33")),
         ])
 
 class WS2812bPhy(Module):
     def __init__(self, rgbled):
-        pixel_bit = Signal(5)
-        pixel_num = Signal(32)
-        max_pixels = Signal(32)
-        delay_counter = Signal(32)
-        pixel = Signal(24, reset=0xff00ff)
-        lo_or_hi = Signal()
+        pixel_bit = Signal(5, reset=24)
+        pixel_num = Signal(5, reset=4)
+        max_pixels = Signal(5, reset=2)
+        delay_counter = Signal(32, reset=0)
+        pixel = Signal(24, reset=0xffffff)
+        current_pixel = Signal(24)
+        do_hi = Signal()
+        delay_counter_reload = Signal(24)
 
-        clk_freq = 150000000
+        clk_freq = 60000000
         T0H = int(0.000400 * clk_freq)
         T1H = int(0.000800 * clk_freq)
         T0L = int(0.000850 * clk_freq)
         T1L = int(0.000450 * clk_freq)
-        RES = int(0.000250 * clk_freq)
+        RES = int(0.5 * clk_freq)
 
-        self.sync += [
-            If(delay_counter > 0,
+        self.sync.sys150 += [
+            If(delay_counter,
                 delay_counter.eq(delay_counter - 1)
-            ).Else(
-                If(pixel_bit >= 24,
-                    If(pixel_num >= max_pixels,
-                        pixel_num.eq(0),
-                        lo_or_hi.eq(0),
-                        delay_counter.eq(RES)
-                    ).Else(
-                        pixel_num.eq(pixel_num + 1),
-                        lo_or_hi.eq(1),
-                        If(pixel[pixel_num + 1],
-                            rgbled.eq(0),
-                            delay_counter.eq(T0H)
-                        ).Else(
-                            rgbled.eq(1),
-                            delay_counter.eq(T1H)
-                        )
-                    )
-                ).Elif(lo_or_hi,
-                    lo_or_hi.eq(0),
-                    pixel_num.eq(pixel_num + 1),
-                    If(pixel[pixel_num],
-                        rgbled.eq(0),
-                        delay_counter.eq(T0L)
-                    ).Else(
-                        rgbled.eq(1),
-                        delay_counter.eq(T1L)
-                    )
+            ).Elif(pixel_bit >= 24,
+#                rgbled.eq(0),
+
+                pixel_bit.eq(0),
+                current_pixel.eq(pixel),
+
+                # If we've run out of pixels on the chain, issue a reset.
+                If(pixel_num >= max_pixels,
+                    delay_counter.eq(RES),
+                    pixel_num.eq(0),
                 ).Else(
-                    lo_or_hi.eq(1),
-                    If(pixel[pixel_num],
-                        rgbled.eq(0),
-                        delay_counter.eq(T0H)
-                    ).Else(
-                        rgbled.eq(1),
-                        delay_counter.eq(T1H)
-                    )
+                    delay_counter.eq(1)
+                )
+                # Otherwise, move on to the next pixel and start over
+                # ).Else(
+                #     pixel_num.eq(pixel_num + 1),
+                #     rgbled.eq(1),
+
+                #     If(pixel[0],
+                #         delay_counter.eq(T1H)
+                #     ).Else(
+                #         delay_counter.eq(T0H)
+                #     )
+                # )
+            # WS2812b is a Hi followed by a Lo.
+            ).Elif(do_hi,
+                do_hi.eq(0),
+                rgbled.eq(1),
+                If(current_pixel[0],
+                    delay_counter.eq(T1H)
+                ).Else(
+                    delay_counter.eq(T0H)
+                )
+            ).Else(
+                do_hi.eq(1),
+                rgbled.eq(0),
+                pixel_bit.eq(pixel_bit + 1),
+                current_pixel.eq(current_pixel >> 1),
+                If(current_pixel[0],
+                    delay_counter.eq(T1L)
+                ).Else(
+                    delay_counter.eq(T0L)
                 )
             )
         ]
@@ -107,11 +129,12 @@ class WS2812bSpecCatcher(Module):
         led = platform.request("user_led")
         rgbled = platform.request("rgbled")
         counter = Signal(26)
-
+        rawclock = platform.request("rawclock")
 
         self.submodules.ClockGen = ClockGen(platform)
         self.submodules.LEDGenerator = WS2812bPhy(rgbled)
         self.comb += led.eq(counter[25])
+        self.comb += rawclock.eq(counter[0])
         self.sync.sys150 += counter.eq(counter + 1)
 
 def main():
